6ad11bf6fb4ed5c83c1fea9c3ab39d96a6791c50
Suzie <queen.suziex@gmail.com>
2025-12-14 13:16:44 -0500

    Finish Wild Canyon data fill. Cleanup set creation using dictionary for hints.

--- sa2-hunting-teacher/Set.cs
+++ sa2-hunting-teacher/Set.cs
@@ -10,5 +10,20 @@
 		public override string ToString() {
 			return $"{Environment.NewLine}	P1: {this.P1}{Environment.NewLine}	P2: {this.P2}{Environment.NewLine}	P3: {this.P3}"; 
 		}
+
+		public static Set Create<T, U, V>(T p1Id, U p2Id, V p3Id, Dictionary<int, string> dict) where T: Enum where U : Enum where V : Enum {
+			return new Set(
+				Convert.ToInt32(p1Id),
+				Convert.ToInt32(p2Id),
+				Convert.ToInt32(p3Id),
+				dict[Set.EnumKey(p1Id)],
+				dict[Set.EnumKey(p2Id)],
+				dict[Set.EnumKey(p3Id)]
+			);
+		}
+
+		public static int EnumKey<T>(T value) where T : Enum {
+			return Convert.ToInt32(value);
+		}
 	}
 }
